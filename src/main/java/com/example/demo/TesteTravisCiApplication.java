0150fc4cad3209325b839704aadbb94d0ddf3491
spfcsandro <spfcsandro@gmail.com>
2018-07-22 14:54:27 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/example/demo/TesteTravisCiApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTravisCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTravisCiApplication.class, args);
+	}
+}
