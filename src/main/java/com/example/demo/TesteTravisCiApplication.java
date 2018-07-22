3f142fdbf7fd1a2f7d84228f7bd4b20fbe566aa7
spfcsandro <spfcsandro@gmail.com>
2018-07-22 14:59:20 -0300

    Alteração no arquivo principal da aplicação

--- src/main/java/com/example/demo/TesteTravisCiApplication.java
+++ src/main/java/com/example/demo/TesteTravisCiApplication.java
@@ -8,5 +8,6 @@ public class TesteTravisCiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteTravisCiApplication.class, args);
+		System.out.println("Teste TravisCI");
 	}
 }
